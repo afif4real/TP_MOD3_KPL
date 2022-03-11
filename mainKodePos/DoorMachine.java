7c9d9a0d551908a14f5dd6cb190431692f752067
afif4real <74724669+afif4real@users.noreply.github.com>
2022-03-11 13:16:38 +0700

    Add files via upload
    initial project

---
+++ mainKodePos/DoorMachine.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mainKodePos;
+
+/**
+ *
+ * @author ASUS
+ */
+public class DoorMachine {
+    private String state = "Terkunci";
+    public void pintu(String command) {
+        command = command.toLowerCase();
+        switch (command) {
+            case "bukapintu":
+                this.state = "Terbuka";
+                break;
+            case "kuncipintu":
+                this.state = "Terkunci";
+                break;
+        }
+    }
+    public String getState(){
+        return this.state;
+        
+    }
+}
---
+++ mainKodePos/mainDoorMachine.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mainKodePos;
+
+/**
+ *
+ * @author ASUS
+ */
+import java.util.Scanner;
+public class mainDoorMachine {
+     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        DoorMachine pintu = new DoorMachine();
+        System.out.println(pintu.getState());
+        System.out.print("Command : ");
+        String c = input.nextLine();
+        pintu.pintu(c);
+        System.out.println(pintu.getState());
+        System.out.print("Command : ");
+        c = input.nextLine();
+        pintu.pintu(c);
+        System.out.println(pintu.getState());
+        input.close();        
+    }
+}
